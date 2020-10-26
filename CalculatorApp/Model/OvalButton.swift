f927b29aeae79975926b7acdb5add9ea23e2d19f
kasrus <kruslim@gmail.com>
2020-10-25 20:00:37 -0700

    first commit

---
+++ CalculatorApp/Model/OvalButton.swift
@@ -0,0 +1,20 @@
+//
+//  OvalButton.swift
+//  CalculatorApp
+//
+//  Created by Kasumi Ruslim on 10/17/20.
+//
+
+import UIKit
+
+class OvalButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CalculatorApp/Model/RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  CalculatorApp
+//
+//  Created by Kasumi Ruslim on 10/16/20.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
